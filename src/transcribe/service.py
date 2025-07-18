b2c0c643626d9c74a6f47b9a7a8bf770b93eac4a
win78py <thangtnsa000@gmail.com>
2025-07-19 01:00:38 +0700

    change model to global

--- src/transcribe/service.py
+++ src/transcribe/service.py
@@ -2,9 +2,8 @@ from src.transcribe.utils import download_temp_audio
 import whisper
 import os
 
+model = whisper.load_model("small")
 def transcribe_from_url(audio_url: str) -> str:
-    model = whisper.load_model("small")
-
     temp_path = download_temp_audio(audio_url)
 
     try:
